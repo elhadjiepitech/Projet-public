b03154d4b2714f454a053cd8a56724e3288aff1a
elhadjiepitech <el-hadji-boubacar.traore@epitech.eu>
2023-10-27 17:21:26 +0200

    task

---
+++ PortFolioReact/1.slider/src/components/Slider/Slider.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import leftChevron from '../../assets/left-arrow.svg'
+import rightChevron from '../../assets/right-arrow.svg'
+import './Slider.css'
+
+export default function Slider() {
+  return (
+    <>
+    <p className="index-info">3/5</p>
+    <div className="slider">
+        <p className="image-info">Bedroom</p>
+        <img src="/public/images/img-3.jpg" alt="estate's room" className='slider-img' />
+
+        <button className="navigation-button prev-button">
+            <img src={leftChevron} alt="previous image" />
+        </button>
+        <button className="navigation-button prev-button">
+            <img src={rightChevron} alt="next image" />
+        </button>
+    </div>
+
+
+    </>
+  )
+}
